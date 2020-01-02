f78a55b958d399559c6c8835318040fcd093d7f0
Brittany Saucedo <44275357+bjs1400@users.noreply.github.com>
2020-01-02 10:41:13 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import HelloWorld from './state-drills/HelloWorld';
+
+import './index.css';
+
+ReactDOM.render(<HelloWorld />, document.getElementById('root'));
